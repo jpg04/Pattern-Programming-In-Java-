50b2c933534f8d32c2fcc4695f25a994ca8ab83a
Jaydeep Godase <80196805+JaydeepGodase@users.noreply.github.com>
2022-11-10 19:55:54 +0530

    Initial Changes

--- RightTriangleStarPattern.java
+++ RightTriangleStarPattern.java
@@ -1,13 +1,12 @@
-public class LeftStart {
+public class RightTriangleStarPattern{
     public static void main(String[] args) {
         int rows=5;
-        for(int i=rows; i>0; i--)
-        {
-            for(int j=1; j<=i; ++j)
-            {
+        for(int i=1; i<=rows; ++i){
+            for(int j=1; j<=i; ++j){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
+
